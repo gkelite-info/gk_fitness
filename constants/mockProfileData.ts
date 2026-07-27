86e458c702c6685b7d3e579a9b94915717411db4
ashishstomar <23213257+ashishstomar@users.noreply.github.com>
2026-07-27 11:12:07 +0530

    feat(constants): add mock profile data

---
+++ constants/mockProfileData.ts
@@ -0,0 +1,31 @@
+export const mockProfileData = {
+  user: {
+    fullName: 'Ashish Kumar',
+    email: 'ashishkumar@gkeliteinfo.com',
+    phone: '+91 98765 43210',
+    gender: 'Male',
+    dateOfBirth: '15 May 1996',
+    avatarUrl: 'https://i.pravatar.cc/150?img=13',
+  },
+  progress: {
+    currentWeight: '72.5 kg',
+    weightChange: '↓ 1.2 kg this week',
+    workoutStreak: '12',
+    streakSuffix: ' Days',
+    streakSubtitle: 'Keep it up!',
+    activeSince: 'May 2024',
+    activeSinceSubtitle: '8 Months',
+  },
+  physical: {
+    height: '175 cm',
+    weight: '72.5 kg',
+    fitnessGoal: 'Weight Gain',
+    activityLevel: 'High',
+  },
+  preferences: {
+    fitnessGoal: 'weight_loss',
+    preferredWorkouts: ['strength', 'cardio', 'yoga'],
+    weeklyTarget: 4,
+    workoutTime: 'evening',
+  },
+};
